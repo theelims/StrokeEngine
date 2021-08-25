5d1b782b2727390112d22b010c782b284c4c9377
elims <46279009+theelims@users.noreply.github.com>
2021-08-25 23:09:12 +0200

    Added pattern RoboStroke, HalfnHalf & Deeper

--- src/pattern.h
+++ src/pattern.h
@@ -16,6 +16,8 @@
 #include <math.h>
 #include "PatternMath.h"
 
+#define DEBUG_PATTERN                 // Print some debug informations over Serial
+
 #ifndef STRING_LEN
   #define STRING_LEN           64     // Bytes used to initalize char array. No path, topic, name, etc. should exceed this value
 #endif
@@ -207,6 +209,217 @@ class TeasingPounding : public Pattern {
         }
 };
 
+
+/**************************************************************************/
+/*!
+  @brief  Robot Stroke Pattern. Sensation controls the acceleration of the
+  stroke. Positive value increase acceleration until it is a constant speed
+  motion (feels robotic). Neutral is equal to simple stroke (1/3, 1/3, 1/3).
+  Negative a reduces acceleration into a triangle profile.
+*/
+/**************************************************************************/ 
+class RoboStroke : public Pattern {
+    public:
+        RoboStroke(char *str) : Pattern(str) {}
+
+        void setTimeOfStroke(float speed = 0) { 
+             // In & Out have same time, so we need to devide by 2
+            _timeOfStroke = 0.5 * speed; 
+        }
+
+        void setSensation(float sensation = 0) { 
+            _sensation = sensation;
+            // scale sensation into the range [0.05, 0.5] where 0 = 1/3
+            if (sensation >= 0 ) {
+              _x = fscale(0.0, 100.0, 1.0/3.0, 0.5, sensation, 0.0);
+            } else {
+              _x = fscale(0.0, -100.0, 1.0/3.0, 0.05, sensation, 0.0);
+            }
+#ifdef DEBUG_PATTERN
+            Serial.println("Sensation:" + String(sensation,0) + " --> " + String(_x,6));
+#endif
+        }
+
+        motionParameter nextTarget(unsigned int index) {
+            // maximum speed of the trapezoidal motion
+            float speed = float(_stroke) / ((1 - _x) * _timeOfStroke);
+            _nextMove.speed = int(speed);  
+
+            // acceleration to meet the profile
+            _nextMove.acceleration = int(speed / (_x * _timeOfStroke));
+
+            // odd stroke is moving out    
+            if (index % 2) {
+                _nextMove.position = _depth - _stroke;
+            
+            // even stroke is moving in
+            } else {
+                _nextMove.position = _depth;
+            }
+
+            _index = index;
+            return _nextMove;
+        }
+    protected:
+        float _x = 1.0/3.0;
+};
+
+/**************************************************************************/
+/*!
+  @brief  Like Teasing or Pounding, but every second stroke is only half the
+  depth. The sensation value can change the speed ratio between in and out. 
+  Sensation > 0 make the in move faster (up to 5x) giving a hard pounding 
+  sensation. Values < 0 make the out move going faster. This gives a more 
+  pleasing sensation. The time for the overall stroke remains the same for
+  all strokes, even half ones. 
+*/
+/**************************************************************************/
+class HalfnHalf : public Pattern {
+    public:
+        HalfnHalf(char *str) : Pattern(str) {}
+        void setSensation(float sensation) { 
+            _sensation = sensation;
+            _updateStrokeTiming();
+        }
+        void setTimeOfStroke(float speed = 0) {
+            _timeOfStroke = speed;
+            _updateStrokeTiming();
+        }
+        motionParameter nextTarget(unsigned int index) {
+            // check if this is the very first 
+            if (index == 0) {
+              //pattern started for the very fist time, so we start gentle with a half move
+              _half = true;
+            }
+
+            // set-up the stroke length
+            int stroke = _stroke;
+            int insert = _depth;
+            if (_half == true) {
+                // half the stroke length
+                stroke = _stroke / 2;
+                // stroke only extends half the insertion depth
+                insert = _depth - stroke;
+            } 
+
+            // odd stroke is moving out
+            if (index % 2) {
+                // maximum speed of the trapezoidal motion
+                _nextMove.speed = int(1.5 * stroke/_timeOfOutStroke);  
+                // acceleration to meet the profile                  
+                _nextMove.acceleration = int(3.0 * float(_nextMove.speed)/_timeOfOutStroke);    
+                _nextMove.position = _depth - _stroke;
+                // every second move is half
+                _half = !_half;
+            // even stroke is moving in
+            } else {
+                // maximum speed of the trapezoidal motion
+                _nextMove.speed = int(1.5 * stroke/_timeOfInStroke);        
+                // acceleration to meet the profile            
+                _nextMove.acceleration = int(3.0 * float(_nextMove.speed)/_timeOfInStroke);    
+                _nextMove.position = insert;  
+            }
+            _index = index;
+            return _nextMove;
+        }
+    protected:
+        float _timeOfFastStroke = 1.0;
+        float _timeOfInStroke = 1.0;
+        float _timeOfOutStroke = 1.0;
+        bool _half = true;
+        void _updateStrokeTiming() {
+            // calculate the time it takes to complete the faster stroke
+            // Division by 2 because reference is a half stroke
+            _timeOfFastStroke = (0.5 * _timeOfStroke) / fscale(0.0, 100.0, 1.0, 5.0, abs(_sensation), 0.0);
+            // positive sensation, in is faster
+            if (_sensation > 0.0) {
+                _timeOfInStroke = _timeOfFastStroke;
+                _timeOfOutStroke = _timeOfStroke - _timeOfFastStroke;
+            // negative sensation, out is faster
+            } else {
+                _timeOfOutStroke = _timeOfFastStroke;
+                _timeOfInStroke = _timeOfStroke - _timeOfFastStroke;
+            }
+#ifdef DEBUG_PATTERN
+            Serial.println("TimeOfInStroke: " + String(_timeOfInStroke));
+            Serial.println("TimeOfOutStroke: " + String(_timeOfOutStroke));
+#endif
+        }
+};
+
+/**************************************************************************/
+/*!
+  @brief  The insertion depth ramps up gradually with each stroke until it
+  reaches its maximum. It then resets and restars. Sensations controls the
+  slope of the ramp. 
+*/
+/**************************************************************************/
+class Deeper : public Pattern {
+    public:
+        Deeper(char *str) : Pattern(str) {}
+
+        void setTimeOfStroke(float speed = 0) { 
+             // In & Out have same time, so we need to devide by 2
+            _timeOfStroke = 0.5 * speed; 
+        }   
+
+        void setSensation(float sensation) { 
+            _sensation = sensation;
+
+            // maps sensation to useful values [2,22] with 12 beeing neutral
+            if (sensation < 0) {
+                _countStrokesForRamp = map(sensation, -100, 0, 2, 11);
+            } else {
+                _countStrokesForRamp = map(sensation, 0, 100, 11, 32);
+            }
+#ifdef DEBUG_PATTERN
+            Serial.println("_countStrokesForrRamp: " + String(_countStrokesForRamp));
+#endif
+        }
+
+        motionParameter nextTarget(unsigned int index) {
+            // How many steps is each stroke advancing         
+            int slope = _stroke / (_countStrokesForRamp);
+
+            // The pattern recycles so we use modulo to get a cycling index.
+            // Factor 2 because index increments with each full stroke twice
+            // add 1 because modulor = 0 is index = 1
+            int cycleIndex = (index / 2) % _countStrokesForRamp + 1;
+
+            // This might be not smooth, as the insertion depth may jump when 
+            // sensation is adjusted.
+
+            // Amplitude is slope * cycleIndex
+            int amplitude = slope * cycleIndex;
+#ifdef DEBUG_PATTERN
+            Serial.println("amplitude: " + String(amplitude)
+                         + " cycleIndex: " + String(cycleIndex));
+#endif
+
+            // maximum speed of the trapezoidal motion 
+            _nextMove.speed = int(1.5 * amplitude/_timeOfStroke);  
+
+            // acceleration to meet the profile
+            _nextMove.acceleration = int(3.0 * _nextMove.speed/_timeOfStroke);
+
+            // odd stroke is moving out    
+            if (index % 2) {
+                _nextMove.position = _depth - _stroke;
+            
+            // even stroke is moving in
+            } else {
+                _nextMove.position = _depth - _stroke + amplitude;
+            }
+
+            _index = index;
+            return _nextMove;
+        }
+    
+    protected:
+        int _countStrokesForRamp = 2;
+
+};
+
 /**************************************************************************/
 /*
   Array holding all different patterns. Please include any custom pattern here.
@@ -214,7 +427,9 @@ class TeasingPounding : public Pattern {
 /**************************************************************************/
 static Pattern *patternTable[] = { 
   new SimpleStroke("Simple Stroke"),
-  new TeasingPounding("Teasing or Pounding")
+  new TeasingPounding("Teasing or Pounding"),
+  new RoboStroke("Robo Stroke"),
+  new HalfnHalf("Half'n'Half")
   // <-- insert your new pattern class here!
  };
 
